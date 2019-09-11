a69166987846842206cf37d0c66b44c26687ce59
Julia <juliawang105@gmail.com>
2019-09-10 19:55:30 -0700

    finished methods according to specs

---
+++ w4d3/skeleton_simon/lib/simon.rb
@@ -0,0 +1,62 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false 
+    @seq = []
+
+  end
+
+  def play
+    self.take_turn until @game_over == true 
+
+    if @game_over == true 
+      self.game_over_message
+      self.reset_game
+    end
+
+  end
+
+  def take_turn
+    self.show_sequence 
+    self.require_sequence
+
+    if @game_over == false
+      self.round_success_message 
+      @sequence_length += 1
+    end
+
+  end
+
+  def show_sequence
+    self.add_random_color
+
+  end
+
+  def require_sequence
+
+  end
+
+  def add_random_color
+    @seq << COLORS.sample
+
+  end
+
+  def round_success_message
+
+  end
+
+  def game_over_message
+
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @seq = []
+    @game_over = false 
+
+  end
+end
